435444292d99189309110f3443bb26ffd51fe24c
SaltyPotato0 <l.stancic.ls@gmail.com>
2024-05-18 20:38:05 +0200

    game works but bad code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoise() {
     let ComputerChoise = Math.floor(Math.random()*3 +1)
     
@@ -10,24 +12,101 @@ function getComputerChoise() {
     else {
         ComputerChoise = "scissors";
     }
+    console.log("Computer choise is: " + ComputerChoise);
     return ComputerChoise;
+   
+
 }
 
-let ComputerChoiceTest = "Computer choise is: " +  getComputerChoise();
-console.log(ComputerChoiceTest);
+
+
+
 
 
 function getUserChoise() {
     let UserChoise = prompt("Choise between rock, paper or scissors");
 
-    if(UserChoise.toLowerCase() == "rock" || UserChoise == "paper" || UserChoise == "sicssors" ){
+    if(UserChoise == "rock" || UserChoise == "paper" || UserChoise == "scissors" ){
         return UserChoise;
         
     }
     else { console.log("pick one of 3"); 
-            getUserChoise(); 
+          return  getUserChoise();
     }
-
-    
 }
 
+
+let ComputerScore = 0;
+let UserScore = 0;
+
+/* Mozda da se promeni sa nekim loopom? */
+function playRounds() {
+    if ( ComputerScore === 3) {
+        alert("computer is the winnder")
+        ComputerScore = 0;
+        UserScore = 0;
+    }
+    else if (UserScore === 3) {
+        alert("Gz u win")
+        ComputerScore = 0;
+        UserScore = 0;
+    }
+
+    else { playRound();}
+}
+
+function playRound() {
+    let ComputerChoise = getComputerChoise();
+    let UserChoise = getUserChoise();
+    console.log("User choise is: " + UserChoise);
+
+    if ( ComputerChoise == "rock" && UserChoise.toLowerCase() == "rock"){
+        console.log("it's a draw, play again");  
+        playRounds();
+    }
+    else if (ComputerChoise == "rock" && UserChoise.toLowerCase() == "paper"){
+        UserScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+
+    else if (ComputerChoise == "rock" && UserChoise.toLowerCase() == "scissors"){
+        ComputerScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+
+    else if ( ComputerChoise == "paper" && UserChoise.toLowerCase() == "paper"){
+        console.log("it's a draw, play again");  
+        playRounds();
+    }
+    else if (ComputerChoise == "paper" && UserChoise.toLowerCase() == "scissors"){
+        UserScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+
+    else if (ComputerChoise == "paper" && UserChoise.toLowerCase() == "rock"){
+        ComputerScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+
+    else if ( ComputerChoise == "scissors" && UserChoise.toLowerCase() == "scissors"){
+        console.log("it's a draw, play again");  
+        playRounds();
+    }
+    else if (ComputerChoise == "scissors" && UserChoise.toLowerCase() == "rock"){
+        UserScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+
+    else if (ComputerChoise == "scissors" && UserChoise.toLowerCase() == "paper"){
+        ComputerScore+=1;
+        console.log("User " + UserScore + " vs " + "Comp " + ComputerScore);
+        playRounds();
+    }
+}
+
+playRound();
